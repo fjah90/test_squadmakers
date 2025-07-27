cdaecb46e528a55485948099fb2be68c156236c5
Fernando Aponte <fernandoaponte90@gmail.com>
2025-07-27 18:19:33 -0400

    feat(api): register MVC controllers and remove WeatherForecast example from Swagger

--- JokesApi/Program.cs
+++ JokesApi/Program.cs
@@ -20,6 +20,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
@@ -105,25 +106,7 @@ app.UseHttpsRedirection();
 
 app.UseMiddleware<JokesApi.Middleware.ErrorHandlingMiddleware>();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 // SEED DB
 using (var scope = app.Services.CreateScope())
